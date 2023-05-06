0fb96037833f57c9fec1f6ea90fa50b73a2b9301
textbrowser <textbrowser@gmail.com>
2023-05-06 19:40:04 -0400

    New files.

---
+++ source/qup.cc
@@ -0,0 +1,66 @@
+/*
+** Copyright (c) 2023, Alexis Megas.
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+** 3. The name of the author may not be used to endorse or promote products
+**    derived from Glitch without specific prior written permission.
+**
+** QUP IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+** QUP, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <QDir>
+#include <QRegularExpression>
+
+#include "qup.h"
+
+QString qup::QUP_VERSION_STRING = "2023.06.01";
+
+qup::qup(void):QMainWindow()
+{
+  m_ui.setupUi(this);
+}
+
+qup::~qup()
+{
+}
+
+QString qup::home_path(void)
+{
+  QString home_path(qgetenv("QUP_HOME").trimmed());
+
+  if(home_path.isEmpty())
+#ifdef Q_OS_WIN32
+    return QDir::currentPath() + QDir::separator() + ".qup";
+#else
+    return QDir::homePath() + QDir::separator() + ".qup";
+#endif
+  else
+    {
+      static auto r
+	(QRegularExpression(QString("[%1%1]+").arg(QDir::separator())));
+
+      home_path.replace(r, QDir::separator());
+
+      if(home_path.endsWith(QDir::separator()))
+	home_path = home_path.mid(0, home_path.length() - 1);
+
+      return home_path;
+    }
+}
